86ea3d99be7299d5112d931c01f4f621d31133bd
Ben Littleton <b.little1996@gmail.com>
2020-11-12 07:37:40 -0500

    Added test lambda to stack

---
+++ hello-cdk/lambdas/hello.js
@@ -0,0 +1,10 @@
+exports.handlers = async (event) => {
+  console.log("Request: ", JSON.stringify(event, undefined, 2));
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: `Hello, CDK! You've hit ${event.path}\n`,
+  };
+};
--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -1,7 +1,14 @@
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambdas"), // directory to look for handler in
+      handler: "hello.handler", // filename.functionname
+    });
   }
 }
