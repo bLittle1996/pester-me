1b91114de4d0ebbad14e067884107d9dc5c6310d
Ben Littleton <b.little1996@gmail.com>
2020-11-12 07:11:13 -0500

    Removed example services in preparation for lambda example

--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -1,18 +1,7 @@
 import * as cdk from "@aws-cdk/core";
-import * as sqs from "@aws-cdk/aws-sqs";
-import * as sns from "@aws-cdk/aws-sns";
-import * as subs from "@aws-cdk/aws-sns-subscriptions";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, "WorkshopQueue", {
-      visibilityTimeout: cdk.Duration.seconds(69),
-    });
-
-    const topic = new sns.Topic(this, "WorkshopTopic");
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
