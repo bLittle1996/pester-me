2a77ca10d712e7d7cc52050729961fb064a5507f
Ben Littleton <b.little1996@gmail.com>
2020-11-12 06:52:41 -0500

    Parity with cdk workshop tutorial.

--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -1,13 +1,16 @@
 import * as cdk from "@aws-cdk/core";
-import * as s3 from "@aws-cdk/aws-s3";
+import * as sqs from "@aws-cdk/aws-sqs";
+import * as sns from "@aws-cdk/aws-sns";
+import * as subs from "@aws-cdk/aws-sns-subscriptions";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "MyVeryOwnBucketOfJoy", {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
+    const queue = new sqs.Queue(this, "WorkshopQueue");
+
+    const topic = new sns.Topic(this, "WorkshopTopic");
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
