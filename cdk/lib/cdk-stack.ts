93175d00118ac62493444c75edd1f1f79a48f33a
Ben Littleton <b.little1996@gmail.com>
2020-11-16 16:25:35 -0500

    Added a user pool with email sign in

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,19 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      signInCaseSensitive: false,
+      // we want email only sign in
+      signInAliases: {
+        email: true,
+        username: false,
+      },
+    });
   }
 }
